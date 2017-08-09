514c9d779ac9b60126903923b660786bde4cd788
ddiazUNSJ <yddiaz@gmail.com>
2017-08-09 08:20:37 -0300

    comenzando con inscripciones a sesiones

---
+++ autentimage/lib/inscriSesion/inscriS.js
@@ -0,0 +1,18 @@
+Schemas.InscriSesionSchema = new SimpleSchema({
+  
+  usuarioId: {
+    type: String,
+    optional: false;
+  }, 
+  nombreUsuario: {
+    type: String,
+    optional: false;
+  }, 
+  idDeGrupo:
+
+  rol: {
+    type: String,
+    optional: false;
+        }
+   
+});
